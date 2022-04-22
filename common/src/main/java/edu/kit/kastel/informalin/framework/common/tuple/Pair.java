1886e3d28dbe4bec77589eb4a75bd9b554c25e8f
Dominik Fuchß <dominik.fuchss@kit.edu>
2022-04-22 10:53:49 +0300

    Add configurabe & common

---
+++ common/src/main/java/edu/kit/kastel/informalin/framework/common/tuple/Pair.java
@@ -0,0 +1,5 @@
+/* Licensed under MIT 2022. */
+package edu.kit.kastel.informalin.framework.common.tuple;
+
+public record Pair<T, U> (T first, U second) {
+}
---
+++ configuration/src/main/java/edu/kit/kastel/informalin/framework/configuration/IConfigurable.java
@@ -0,0 +1,8 @@
+/* Licensed under MIT 2022. */
+package edu.kit.kastel.informalin.framework.configuration;
+
+import java.util.Map;
+
+public interface IConfigurable {
+    void applyConfiguration(Map<String, String> additionalConfiguration);
+}
