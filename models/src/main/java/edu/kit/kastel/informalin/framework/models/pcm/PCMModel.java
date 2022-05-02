232fec501a51e59d59e7f95f1071ac7fd94c80c1
Dominik Fuchß <dominik.fuchss@kit.edu>
2022-05-02 11:15:43 +0300

    More constructors for PCMModel

--- models/src/main/java/edu/kit/kastel/informalin/framework/models/pcm/PCMModel.java
+++ models/src/main/java/edu/kit/kastel/informalin/framework/models/pcm/PCMModel.java
@@ -3,20 +3,18 @@ package edu.kit.kastel.informalin.framework.models.pcm;
 
 import org.fuchss.xmlobjectmapper.XML2Object;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Objects;
+import java.io.*;
 
 public class PCMModel {
-    private final File repositoryFile;
-
     private PCMRepository repository;
 
-    public PCMModel(File repository) {
-        this.repositoryFile = Objects.requireNonNull(repository);
+    public PCMModel(File repository) throws IOException {
+        this(new FileInputStream(repository));
+    }
+
+    public PCMModel(InputStream repositoryStream) {
         try {
-            this.load();
+            this.load(repositoryStream);
         } catch (ReflectiveOperationException | IOException e) {
             throw new IllegalArgumentException(e.getMessage(), e);
         }
@@ -26,12 +24,10 @@ public class PCMModel {
         return repository;
     }
 
-    private void load() throws ReflectiveOperationException, IOException {
+    private void load(InputStream repositoryStream) throws ReflectiveOperationException, IOException {
         XML2Object xml2Object = new XML2Object();
         xml2Object.registerClasses(PCMRepository.class, PCMComponent.class, PCMInterface.class, PCMSignature.class, PCMComponent.InterfaceId.class);
-        try (var repoStream = new FileInputStream(this.repositoryFile)) {
-            repository = xml2Object.parseXML(repoStream, PCMRepository.class);
-            repository.init();
-        }
+        repository = xml2Object.parseXML(repositoryStream, PCMRepository.class);
+        repository.init();
     }
 }
